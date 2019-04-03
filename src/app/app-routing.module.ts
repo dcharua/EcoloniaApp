0d4bf909f15c6381b432713bd8b208452b9c05a1
JoanAndoni <mcr_joan@hotmail.com>
2019-04-03 13:05:17 -0600

    Navigation and all pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // SHARED PAGES
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'user-home', loadChildren: './user/home/home.module#HomePageModule' },
-  { path: 'user-coupon', loadChildren: './user/coupon/coupon.module#CouponPageModule' },
+
+  // ADMIN PAGES
   { path: 'admin-home', loadChildren: './admin/home/home.module#HomePageModule' },
   { path: 'admin-coupon', loadChildren: './admin/coupon/coupon.module#CouponPageModule' },
+  { path: 'admin-images', loadChildren: './admin/images/images.module#ImagesPageModule' },
+
+  // USER PAGES
+  { path: 'user-home', loadChildren: './user/home/home.module#HomePageModule' },
+  { path: 'user-coupon', loadChildren: './user/coupon/coupon.module#CouponPageModule' },
   { path: 'user-profile', loadChildren: './user/profile/profile.module#ProfilePageModule' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'camera', loadChildren: './user/camera/camera.module#CameraPageModule' }
+  { path: 'user-camera', loadChildren: './user/camera/camera.module#CameraPageModule' }
 ];
 
 @NgModule({
