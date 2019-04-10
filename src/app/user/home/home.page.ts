1d2ca2b2cb7fd98b4656dc0813489a5eb216ef01
JoanAndoni <mcr_joan@hotmail.com>
2019-04-09 22:57:18 -0500

    Display the published by day and all done by now

--- src/app/user/home/home.page.ts
+++ src/app/user/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../../shared/services/photo.service';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  numberPhotos: number = 0;
+  numberPhotosToday: number = 0;
 
-  constructor() { }
+  constructor(
+    public photoService: PhotoService,
+    public authService: AuthService
+  ) {
+    this.photoService.getPhotos().subscribe((photos) => {
+      this.numberPhotos = photos.length;
+      const today = new Date();
+      photos.forEach((photo, index) => {
+        let compare = new Date(photo.createdOn);
+        if (compare.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0)) {
+          console.log("Uploaded today");
+        } else {
+          photos.slice(index, 1);
+        }
+      });
+      this.numberPhotosToday = photos.length;
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
